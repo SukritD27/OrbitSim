ccf44750f503d3fd2241f946a3e4d7964c003ab8
SukritD27 <sdhawan@sfsu.edu>
2023-10-26 16:51:59 -0700

    The bodies can move now

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import math
 import turtle
 
 class CosmicBodies(turtle.Turtle):
-    min_display_size = 50
+    min_display_size = 20
     # used to convert mass to display size
     log_base = 1.1
 
@@ -12,7 +12,8 @@ class CosmicBodies(turtle.Turtle):
         self.mass = mass
         # self.forward(100)
         self.setposition(position)
-        # self.velocity(velocity)
+        
+        self.velocity = velocity
 
         self.display_size = max(math.log(self.mass, self.log_base), self.min_display_size)
 
@@ -24,13 +25,18 @@ class CosmicBodies(turtle.Turtle):
         space.add_body(self)
 
     def draw(self):
+        self.clear()
         self.dot(self.display_size)
         # space.update()
         # space.mainloop()
-            # self.forward(100)
+        # self.forward(100)
+    
+    def move(self):
+        self.setx(self.xcor() + self.velocity[0])
+        self.sety(self.ycor() + self.velocity[1])
 
 class Sun(CosmicBodies):
-    def __init__(self, space, mass, position=(0,0), velocity=(0,0),):
+    def __init__(self, space, mass, position=(0,0), velocity=(0,0)):
         super().__init__(space, mass, position, velocity)
         self.color('orange')
 
@@ -41,7 +47,9 @@ class Sun(CosmicBodies):
 class Space:
     def __init__(self, height, width):
         self.space = turtle.Screen()
-        # self.space.tracer(0)
+        # tracer(0) turns the screen updates off
+        # stops the screen from blinking
+        self.space.tracer(0)
         self.space.setup(width, height)
         self.space.bgcolor('black')
         # self.space.title('Space')
@@ -52,4 +60,10 @@ class Space:
 
     def remove_body(self, body):
         self.bodies.remove(body)
+
+    def update_all_bodies(self):
+        for body in self.bodies:
+            body.move()
+            body.draw()
+        self.space.update()
 
--- src/solar_system.py
+++ src/solar_system.py
@@ -1,7 +1,11 @@
 from main import Space, Sun
 
-solar_system = Space(height=9000,width=14000)
-sun = Sun(solar_system, mass=10_0000)
+solar_system = Space(height=900,width=1400)
+sun = Sun(solar_system, mass=10_000, velocity=(90,1))
+
+while True:
+    solar_system.update_all_bodies()
+
 sun.draw()
 
 import turtle
